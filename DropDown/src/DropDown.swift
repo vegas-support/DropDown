0bd706fae6bd96f45044fdd34b4e172a09230b72
Rudy FREMONT <rudy.fremont@orange.com>
2020-12-10 17:50:15 +0100

    Revert "Fix package bundle"
    This reverts commit 97d3a4a23fe46134e7c5f9a49d9f262400487bf2.

--- DropDown/src/DropDown.swift
+++ DropDown/src/DropDown.swift
@@ -355,7 +355,7 @@ public final class DropDown: UIView {
      
      Changing the cell nib automatically reloads the drop down.
      */
-	public var cellNib = UINib(nibName: "DropDownCell", bundle: Bundle.module) {
+	public var cellNib = UINib(nibName: "DropDownCell", bundle: Bundle(for: DropDownCell.self)) {
 		didSet {
 			tableView.register(cellNib, forCellReuseIdentifier: DPDConstant.ReusableIdentifier.DropDownCell)
 			templateCell = nil
