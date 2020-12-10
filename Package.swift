89e9b5f4673ffdbb5010fb5eb991480260058029
Rudy FREMONT <rudy.fremont@orange.com>
2020-12-10 17:50:03 +0100

    revert

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-/*let package = Package(
+let package = Package(
     name: "DropDown",
     platforms: [ .iOS(.v9) ],
     products: [
@@ -13,19 +13,7 @@ import PackageDescription
             path: "DropDown",
             exclude: ["Info.plist", "DropDown.h"],
             resources: [
-                .process("resources/DropDownCell.xib")
+                .process("resources")
             ])
     ]
-)*/
-
-let package =   Package(
-  name: "DropDown",
-  platforms: [.iOS(.v9)],
-  products: [
-    .library(name: "DropDown", targets: ["DropDown"]),
-  ],
-  targets: [
-    .target(name: "DropDown", path: "DropDown")
-  ],
-  swiftLanguageVersions: [.v5]
 )
